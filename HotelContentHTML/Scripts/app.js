1b1fe1fa33c33031f4a0dee5aa3c66219ce7409c
vtech-virat <vtech.virat@gmail.com>
2015-11-07 12:59:31 +0530

    File added

---
+++ HotelContentHTML/Scripts/app.js
@@ -0,0 +1,31 @@
+var hotelcontentApp = angular.module("hotelcontentApp", ["ngRoute"]);
+
+hotelcontentApp.config(function($routeProvider) {
+    $routeProvider.
+      when("/hotellist", {
+        templateUrl: "HotelList.html",
+        controller: "hotelListcontroller"
+      }).
+      when("/newhotel",
+      {
+          templateUrl:"HotelDetails.html",
+          controller:"hoteldetailscontroller"
+      }).otherwise({
+        redirectTo: "/hotellist"
+      });
+        });
+
+hotelcontentApp.controller("hotelListcontroller", function($scope,$http)
+{
+	$http.get('json/HotelList.json').success(function(data)
+		{
+			$scope.hotellist=data.records;
+		});
+});
+
+
+hotelcontentApp.controller("hoteldetailscontroller", function($scope)
+{
+  $scope.Id=100;
+});
+
